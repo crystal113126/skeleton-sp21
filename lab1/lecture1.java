07fe040cdd00f9709e3bc5f71665fa0e2cb2f130
Jingia <jingx113126@gmail.com>
2022-07-08 15:39:53 -0400

    finished Collatz function and tested and Add lecture1 class

---
+++ lab1/lecture1.java
@@ -0,0 +1,9 @@
+public class lecture1 {
+    public static void makeNoise() {
+        System.out.println(" Haha!");
+    }
+    public static void main(String[] args) {
+        lecture1.makeNoise();
+    }
+}
+
